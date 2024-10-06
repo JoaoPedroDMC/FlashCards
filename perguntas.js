b56d564e3029c7e0f573510ac5443e2ce30ea257
João Pedro <168283243+JoaoPedroDMC@users.noreply.github.com>
2024-10-05 22:34:30 -0300

    1

---
+++ perguntas.js
@@ -0,0 +1,16 @@
+criaCartao(
+    'Geografia', // Tipo
+    'Qual é a capital da Rússia?', // Pergunta
+    'Moscou' // Resposta
+);
+criaCartao(
+    'Matemática', // Tipo
+    'Qual é 2 + 2?', // Pergunta
+    '4' // Resposta
+);
+criaCartao(
+    'História', // Tipo
+    'Quem descobriu o Brasil?', // Pergunta
+    'Pedro Álvares Cabral' // Resposta
+);
+
